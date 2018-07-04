351ed1173651680992199a624c47ef230393430a
yinzuolong <yinzuolong@ppdai.com>
2018-07-04 13:43:56 +0800

    新建

---
+++ src/main/java/com/along101/framework/hystrix/Application.java
@@ -0,0 +1,24 @@
+package com.along101.framework.hystrix;
+
+import com.along101.framework.hystrix.turbine.EnableTurbine;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableTurbine
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/hs")
+	public String hs(){
+		return "OK";
+	}
+}
